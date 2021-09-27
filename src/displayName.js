6d434ee0a5273e6881ebd404c093649b902c08d6
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-09-27 20:42:01 +0200

    displayName exercise

---
+++ src/displayName.js
@@ -0,0 +1,15 @@
+//Napisz funkcję displayName, która przyjmuje dwa argumenty, imię i nazwisko, i wyświetla je w powitaniu w stylu Jamesa Bonda. 
+//Spójrz na przypadki testowe, aby zrozumieć jego zasadę działania. Twoja funkcja musi działać tak samo! Oto kilka przypadków testowych:
+
+
+function displayName(name, surname){
+    console.log(`My name is ${surname}, ${name} ${surname}`)
+    
+}
+
+
+displayName("James", "Bond")   // My name is Bond, James Bond
+
+displayName("Ada", "Lovelace") // My name is Lovelace, Ada Lovelace
+
+
