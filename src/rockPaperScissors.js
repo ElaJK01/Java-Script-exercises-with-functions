ae6bb309468e9e8dd62310660760bd348e4d8e16
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-09-27 21:45:32 +0200

    rockPaperScissors to lowercase string

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -1,6 +1,8 @@
 
 
 function rockPaperScissors(firstPlayer, secondPlayer){
+    firstPlayer = firstPlayer.toLowerCase()
+    secondPlayer = secondPlayer.toLowerCase()
     if (firstPlayer == "rock" && secondPlayer == "rock" || firstPlayer == "scissors" && secondPlayer == "scissors" || 
     firstPlayer == "paper" && secondPlayer == "paper"){
         console.log("Draw")
@@ -17,3 +19,4 @@ rockPaperScissors("rock", "rock")      // Draw
 rockPaperScissors("rock", "paper")     // Second player wins
 rockPaperScissors("rock", "scissors")  // First player wins
 rockPaperScissors("paper", "scissors") // Second player wins
+rockPaperScissors("PAPER", "scissors") // Second player wins
